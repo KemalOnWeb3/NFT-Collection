669ca74cfd18cd079a62d4bad11758420e7c601f
Kemal Cleva <68566006+KemalOnWeb3@users.noreply.github.com>
2022-05-26 23:13:43 +0200

    y

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -580,5 +580,5 @@ export const abi = [
       "stateMutability": "payable",
       "type": "receive"
     }
-  ]
+  ];
 export const NFT_CONTRACT_ADDRESS = "0x0353eF3ADc48559d8dDb782599bb614B93E24f56"
